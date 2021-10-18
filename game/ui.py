e607ddd8b0d9ed21b8e0455a488d9a443d815f09
Thierry Chappuis <tchappui@gmail.com>
2021-10-18 20:02:34 +0200

    Initial object-oriented version

--- game/ui.py
+++ game/ui.py
@@ -3,98 +3,99 @@
 import pygame
 
 import config
-from . import engine
+from .engine import Board
 
 pygame.init()
 
 BLOCK_DOWN_EVENT = pygame.USEREVENT + 0
 
 
-def create_app():
-    """Crée une nouvelle application."""
-    pygame.time.set_timer(BLOCK_DOWN_EVENT, config.UI_TIMER, True)
-    pygame.key.set_repeat(config.UI_KEY_REPEAT_DELAY)
-    return {
-        'screen': pygame.display.set_mode(config.UI_SCREEN_SIZE),
-        'timer': config.UI_TIMER,
-        'board': engine.create_board(),
-        'clock': pygame.time.Clock(),
-    }
+class Application:
+    """Représente l'interface graphique du jeu."""
 
+    def __init__(self):
+        """Initialise une nouvelle application."""
+        pygame.display.set_caption(config.UI_TITLE)
+        pygame.time.set_timer(BLOCK_DOWN_EVENT, config.UI_TIMER, True)
+        pygame.key.set_repeat(config.UI_KEY_REPEAT_DELAY)
 
-def run(app):
-    """Démarre l'interface graphique du jeu et gère la boucle événementielle."""
-    app['running'] = True
-    while app['running']:
-        _handle_events(app)
-        _refresh_screen(app)
-    pygame.quit()
+        self._screen = pygame.display.set_mode(config.UI_SCREEN_SIZE)
+        self._timer = config.UI_TIMER
+        self._board = Board()
+        self._clock = pygame.time.Clock()
 
+        self.running = False
 
-def stop(app):
-    """Arrête l'application."""
-    app['running'] = False
+    def run(self):
+        """Démarre l'interface graphique du jeu et gère la boucle
+        événementielle."""
+        self.running = True
+        while self.running:
+            self._handle_events()
+            self._refresh_screen()
+        pygame.quit()
 
+    def stop(self):
+        """Arrête l'application."""
+        self.running = False
 
-def _handle_events(app):
-    """Gère les événements reçu par l'application."""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            stop(app)
+    def _handle_events(self):
+        """Gère les événements reçu par l'application."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.stop()
 
-        elif event.type == BLOCK_DOWN_EVENT:
-            pygame.time.set_timer(BLOCK_DOWN_EVENT, app['timer'], True)
-            engine.move_down(app['board'])
+            elif event.type == BLOCK_DOWN_EVENT:
+                pygame.time.set_timer(BLOCK_DOWN_EVENT, self._timer, True)
+                self._board.move('down')
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                engine.move_left(app['board'])
-            elif event.key == pygame.K_RIGHT:
-                engine.move_right(app['board'])
-            elif event.key == pygame.K_DOWN:
-                engine.move_down(app['board'])
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self._board.move('left')
+                elif event.key == pygame.K_RIGHT:
+                    self._board.move('right')
+                elif event.key == pygame.K_DOWN:
+                    self._board.move('down')
 
+    def _draw_block(self, block, color):
+        """Dessine un block sur l'écran."""
+        x, y = block
+        pygame.draw.rect(
+            self._screen,
+            color,
+            (
+                x * config.UI_BLOCK_SIZE,
+                y * config.UI_BLOCK_SIZE,
+                config.UI_BLOCK_SIZE,
+                config.UI_BLOCK_SIZE,
+            ),
+        )
 
-def _draw_block(block, color):
-    """Dessine un block sur l'écran."""
-    x, y = block
-    screen = pygame.display.get_surface()
-    pygame.draw.rect(
-        screen,
-        color,
-        (
-            x * config.UI_BLOCK_SIZE,
-            y * config.UI_BLOCK_SIZE,
-            config.UI_BLOCK_SIZE,
-            config.UI_BLOCK_SIZE,
-        ),
-    )
+    def _refresh_screen(self):
+        """Mets à jour l'interface utilisation en fonction des mises à jour du
+        back-end."""
+        # On met à jour le back-end: la logique du jeu se déroule ici
+        block_created, game_over = self._board.update()
 
+        # On dessine le fond de l'écran
+        self._screen.fill(config.UI_BACKGROUND_COLOR)
 
-def _refresh_screen(app):
-    """Mets à jour l'interface utilisation en fonction des mises à jour du back-end"""
-    # On met à jour le back-end
-    current, blocks, block_created, game_over = engine.update(app['board'])
+        # On dessine le block manipulé par l'utilisateur
+        self._draw_block(self._board.current, config.UI_BLOCK_CURRENT_COLOR)
 
-    # On dessine le fond de l'écran
-    app['screen'].fill(config.UI_BACKGROUND_COLOR)
+        # On dessine les anciens blocs encore visibles
+        for block in self._board:
+            self._draw_block(block, config.UI_BLOCK_OLD_COLOR)
 
-    # On dessine le block manipulé par l'utilisateur
-    _draw_block(current, config.UI_BLOCK_CURRENT_COLOR)
+        # Lorsqu'un nouveau bloc est créé, on accélère le jeu
+        if block_created:
+            timer = self._timer * config.UI_TIMER_ACCELERATION
+            self._timer = max(int(timer), config.UI_TIMER_MIN)
 
-    # On dessine les anciens blocs encore visibles
-    for block in blocks:
-        _draw_block(block, config.UI_BLOCK_OLD_COLOR)
+        # Lorsqu'un bloc atteint le sommet du plateau, on abdique
+        if game_over:
+            self.stop()
 
-    # Lorsqu'un nouveau bloc est créé, on accélère le jeu
-    if block_created:
-        timer = app['timer'] * config.UI_TIMER_ACCELERATION
-        app['timer'] = max(int(timer), config.UI_TIMER_MIN)
-
-    # Lorsqu'un bloc atteint le sommet du plateau, on abdique
-    if game_over:
-        stop(app)
-
-    # On rafraîchit l'écran et contrôle la vitesse de la boucle de jeu
-    pygame.display.update()
-    app['clock'].tick(config.UI_FRAME_RATE)
+        # On rafraîchit l'écran et contrôle la vitesse de la boucle de jeu
+        pygame.display.update()
+        self._clock.tick(config.UI_FRAME_RATE)
